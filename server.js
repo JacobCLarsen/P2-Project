0325a97bed4b57c6988db94aa76987477c8c7ea8
JacobCLarsen <jcla24@student.aau.dk>
2025-03-06 10:35:27 +0100

    Socket.io emits message to all open browsers

--- server.js
+++ server.js
@@ -19,8 +19,13 @@ io.on("connection", (socket) => {
 */
 
 io.on("connection", (socket) => {
+  console.log("a user connected");
   socket.on("chat message", (msg) => {
     console.log("message: " + msg);
+    io.emit("chat message", msg);
+  });
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
   });
 });
 
