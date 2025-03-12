a9156fa6db8d1762d195b2ae8e606920a1c561a7
JacobCLarsen <jcla24@student.aau.dk>
2025-03-12 14:25:34 +0100

    Split the login page into js, css and html files

---
+++ public/login.js
@@ -0,0 +1,42 @@
+const users = [
+  { username: "Daniel", password: "123" },
+  { username: "Jacob", password: "345" },
+  { username: "Philip", password: "567" },
+  { username: "Anders", password: "789" },
+  { username: "Zet", password: "1234" },
+];
+
+function validateLogin(event) {
+  event.preventDefault(); // Prevent the browser's default form submission
+
+  const enteredUsername = document.getElementById("username").value;
+  const enteredPassword = document.getElementById("password").value;
+  const message = document.getElementById("message");
+
+  // Clear the error message each time user tries to submit
+  message.textContent = "";
+
+  // Find the user with matching credentials
+  const CorrectUser = users.find(
+    (user) =>
+      user.username === enteredUsername && user.password === enteredPassword
+  );
+
+  // Show success or error message
+  if (CorrectUser) {
+    message.style.color = "green";
+    message.textContent = "Login Successful!";
+
+    // Redirect to the homepage (root route)
+    setTimeout(() => {
+      // Timeout for dramatic effect
+      window.location.href = "/"; // Redirect to the homepage
+    }, 500);
+  } else {
+    message.style.color = "red";
+    message.textContent = "Invalid Username or Password! Please try again.";
+
+    // Focus the username or password field to guide the user
+    document.getElementById("username").focus();
+  }
+}
