aa494f7cd63d85ef22d4604d632c69b920de0125
Daniel Sloth <131558433+dani156f@users.noreply.github.com>
2025-03-13 14:18:17 +0100

    copy to clipboard

--- public/hash-converter.js
+++ public/hash-converter.js
@@ -5,10 +5,17 @@ console.log(typeof hashSHA512);
 async function convertedPassword() {
     let password = document.getElementById("password").value;
     let hashed = await hashSHA512(password);
-    let output = document.getElementById("output");
-
+   
+    let output = document.getElementById("output-box");
     output.style.display = "block";
+
     document.getElementById("output").innerText = "Hashed Output: " + hashed;
 }
 
 document.querySelector(".btn").addEventListener("click", convertedPassword);
+
+// Copy to clipboard button
+document.getElementById("copy-btn").addEventListener("click", function () {
+    let hashText = document.getElementById("output").innerText;
+    navigator.clipboard.writeText(hashText)
+});
