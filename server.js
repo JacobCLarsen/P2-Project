8af1b410042a8e0c568b28549a6aac8e4bfcf0bd
PhilipHellerupAAU <pjorge24@student.aau.dk>
2025-03-19 12:46:45 +0100

    Added Logic Places in server.js

--- server.js
+++ server.js
@@ -1,21 +1,22 @@
-/* Imports:
- * Express for API's/ routing
- * Socket.io (Websockets) are used for real-time communication between the clients and the server
- * Router for handling webpage routes
- * Database connection and setup utilities
- */
+/* ----- IMPORTS ----- */
+
+// Core modules
+import path from "path";
+import { fileURLToPath } from "url";
+
+// Third-party modules
 import express from "express";
 import { createServer } from "http";
 import { Server } from "socket.io";
+
+// Custom modules
 import router from "./router.js";
 import { setupSocketCommunication } from "./setupSocketCommunication.js";
 import { setupAuth } from "./setupAuth.js";
-import path from "path";
-import { fileURLToPath } from "url";
 import DBConnection, {
   connectToDatabase,
   setupDatabaseRoutes,
-} from "./databaseConnection.js"; // Import DB connection function and insertTestData
+} from "./databaseConnection.js";
 
 // Define the path of the current file and directory:
 const __filename = fileURLToPath(import.meta.url);
@@ -25,36 +26,43 @@ const __dirname = path.dirname(__filename);
 const app = express();
 const server = createServer(app);
 
-// Create socket.io Server for WebSocket communication:
-const io = new Server(server);
 
-// Use the router for handling routes:
-app.use("/", router);
 
-// Serve static files from the "public" directory:
+/* ----- MIDDLEWARE ----- */
+
+// Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, "public")));
 
+// Middleware to parse JSON payloads in incoming requests
+app.use(express.json());
+
+// Use the router for handling routes
+app.use("/", router);
+
 // Set up WebSocket communication:
+const io = new Server(server);
 setupSocketCommunication(io);
 
 // Set up authentication routes (e.g., login/signup):
 setupAuth(app);
 
-// Connect to the database and initialize tables:
+// Database setup:
+// Connect to the database and initialize tables
 connectToDatabase();
 
 // Set up database-related routes
 setupDatabaseRoutes(app);
 
-// Middleware to parse JSON payloads in incoming requests:
-app.use(express.json());
 
-// Simple test route to verify server is running:
+
+/* ----- ROUTES ----- */
+
+// Simple test route to verify server is running
 app.get("/", (req, res) => {
   res.send("Server is running!");
 });
 
-// Test WebSocket proxy route (example usage):
+// Test WebSocket proxy route (example usage)
 app.use("/ws0", (req, res) =>
   proxy.web(req, res, { target: "ws://localhost:4310" })
 );
