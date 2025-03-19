66213d254381efebe4ef5ae86c16589f55767fbd
Daniel Sloth <131558433+dani156f@users.noreply.github.com>
2025-03-19 15:19:30 +0100

    extension

--- public/chrome-extension.js
+++ public/chrome-extension.js
@@ -1,27 +1,14 @@
 // Get startBtn element
 const startBtn = document.getElementById("startBtn");
-const dots = document.getElementById("dots");
-
 // Event listener for startBtn to start and stop work
 startBtn.addEventListener("click", function () {
-    if (startBtn.innerHTML == "Click to start working") {
+    if (startBtn.innerHTML === "Click to start working") {
         startBtn.innerHTML = "Hashing passwords ...";
         startBtn.style.color = "green";
         startBtn.style.border = "2px solid green";
-    }   else {
+    } else {
         startBtn.innerHTML = "Click to start working";
+        startBtn.style.color = "";
+        startBtn.style.border = "";
     }
 });
-
-function dotsAnimation(){
-let dots = "";
-  const interval = setInterval(() => {
-    if (startBtnText.innerText !== "Hashing hashes") {
-      clearInterval(interval);
-      startBtnLoad.innerText = "";
-      return;
-    }
-    dots = dots.length < 4 ? dots + "." : ".";
-    startBtnLoad.innerText = dots;
-  }, 600);
-}
