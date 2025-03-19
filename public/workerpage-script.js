aa5d9070d71d81f6b9727409c4c6a890e5c7b7dc
JacobCLarsen <jcla24@student.aau.dk>
2025-03-19 15:16:05 +0100

    worker keeps working

--- public/workerpage-script.js
+++ public/workerpage-script.js
@@ -38,6 +38,19 @@ mySocket.onmessage = (event) => {
   }
 };
 
+// Send a message to the server to fetch a task
+function fetchTask() {
+  startWorkUI();
+  let message = {
+    action: "request task",
+    data: null,
+    id: clientId,
+  };
+
+  // When the server receives the message, it will send a task back to the client which will be received in the onmessage eventlistener above
+  mySocket.send(JSON.stringify(message));
+}
+
 // Start working involves, fetching a task, creating a worker to complete the task and sending the result back to the server
 function startWork(task) {
   const myWorker = new Worker("worker.js");
@@ -63,7 +76,9 @@ function startWork(task) {
 
   // for demo - weight 2 seconds before fetching a new task
   setTimeout(() => {
-    fetchTask();
+    if (startBtnText.innerText == "Hashing passwords") {
+      fetchTask();
+    }
   }, 2000);
 }
 
@@ -72,6 +87,7 @@ function startWorkUI() {
   // Distaplay the message "Please dont leave the page when working, and also adds a beforeunloadHandeler to the window
   window.addEventListener("beforeunload", beforeReloadHandeler);
   messageBox.style.display = "block";
+  startBtnText.innerText = "Hashing passwords";
 
   // Loading function to the
   let dots = "";
@@ -89,6 +105,12 @@ function startWorkUI() {
   startBtn.style.border = "#007bff solid 2px";
   startBtnText.style.color = "#007bff";
   startBtnLoad.style.color = "#007bff";
+
+  // Navigation links should open in a new window
+  document.querySelectorAll("a").forEach((link) => {
+    link.setAttribute("target", "_blank");
+    link.setAttribute("rel", "noopener noreferrer");
+  });
 }
 
 function stopWork() {
@@ -100,22 +122,18 @@ function stopWorkUI() {
   window.removeEventListener("beforeunload", beforeReloadHandeler);
   // Hide the messsage box
   messageBox.style.display = "none";
+  startBtnText.innerText = "Click to start working";
+  startBtnLoad.innerText = "";
   // Revert to default (hover-only)
   startBtn.style.border = "";
   startBtnText.style.color = "";
   startBtnLoad.style.color = "";
-}
 
-// Send a message to the server to fetch a task
-function fetchTask() {
-  let message = {
-    action: "request task",
-    data: null,
-    id: clientId,
-  };
-
-  // When the server receives the message, it will send a task back to the client which will be received in the onmessage eventlistener above
-  mySocket.send(JSON.stringify(message));
+  // Navigation links can now open in the same window
+  document.querySelectorAll("a").forEach((link) => {
+    link.removeAttribute("target", "_blank");
+    link.removeAttribute("rel", "noopener noreferrer");
+  });
 }
 
 // Alert the user when they try to leave to reload the page while working. This function is added to the workbutton eventlistener
@@ -126,131 +144,8 @@ function beforeReloadHandeler(event) {
 // clicking "Start working " will start work form this client
 startBtn.addEventListener("click", function () {
   if (startBtnText.innerText == "Click to start working") {
-    startBtnText.innerText = "Hashing passwords";
     fetchTask();
-    startWorkUI();
   } else {
-    startBtnText.innerText = "Click to start working";
-    startBtnLoad.innerText = "";
     stopWork();
   }
 });
-
-/*
-// Starting work will fetch a task
-function startWork() {
-  workStartedUI();
-  fetchTask();
-}
-
-// When work stops, emit it to the server
-function stopWork(result) {
-  workStoppedUI();
-  socket.emit("stop work");
-}
-
-// To fetch a task, we use socket.emit to send a message to the server asking for a task
-async function fetchTask() {
-  socket.emit("request task", "A worker wants to do a task");
-}
-
-// When the client recieves a message with an "assigned task", it will use the data send in the message and complete the task
-socket.on("assigned task", (task) => {
-  completeTask(task);
-});
-
-// Completing a task is done by initializing a worker, passing data to the worker using .postmessage and receive the data using .onmessage.
-function completeTask(task) {
-  const myWorker = new Worker("worker.js");
-  console.log("worker connected!");
-  myWorker.postMessage(task.value);
-  console.log(`Message containing ${task.value} was send to the a worker`);
-
-  myWorker.onmessage = (e) => {
-    console.log("Message received from worker:", e.data);
-    myWorker.terminate();
-
-    let item = document.createElement("li");
-    item.innerText = `You completed task ${task.value} with result ${e.data}`;
-    taskList.innerHTML = "";
-    taskList.append(item);
-    socket.emit(
-      "complete task",
-      `Task ${task.value} completed with result: ${e.data} from node: ${task.username} with id: ${socket.id}`
-    );
-  };
-
-  // Start working again
-  setTimeout(() => {
-    if (startBtnText.innerText === "Hashing hashes ...") {
-      fetchTask();
-    }
-  }, "2000");
-}
-
-// When a task i completed by another client, it is send to all clients so show on the GUI
-socket.on("task result", (msg) => {
-  let result = document.createElement("li");
-  result.innerText = msg;
-  taskResults.append(result);
-});
-
-// Function to open af new tab to work on antoher page
-plusTabBtn.addEventListener("click", function () {
-  window.open("startwork", "_blank");
-});
-
-// ************ Cosmetic functions ************
-// Function to handle UI changes on work start
-function workStartedUI() {
-  startBtnText.innerText = "Hashing hashes ...";
-  startBtnText.style.color = "#007bff";
-  startBtn.style.border = "2px solid #007bff";
-  // Add eventlistener to alert the user before leaving the page
-  window.addEventListener("beforeunload", beforeReloadHandeler);
-  // When working navigation to a new page should open it in a new window
-  document.querySelectorAll("a").forEach((link) => {
-    link.setAttribute("target", "_blank");
-    link.setAttribute("rel", "noopener noreferrer");
-  });
-  plusTabBtn.style.display = "block";
-}
-
-// Function to handle UI changes on work stop
-function workStoppedUI() {
-  startBtnText.innerText = "Click to start working";
-  startBtnText.style.color = "#444";
-  startBtn.style.border = "2px solid #444";
-  hashingText.innerText = "";
-  // The user is safe to leave the page, as no work is being done
-  window.removeEventListener("beforeunload", beforeReloadHandeler);
-  // When not working, tabs are opened in the same window
-  document.querySelectorAll("a").forEach((link) => {
-    link.removeAttribute("target", "_blank");
-    link.removeAttribute("rel", "noopener noreferrer");
-  });
-  plusTabBtn.style.display = "none";
-}
-
-// Function to animate the hashing text
-const letters = "ABCTUVWXYZ$#!%&/()=?*";
-let interval = null;
-
-function hashAnimation() {
-  hashingText.innerText = "0000000000";
-  let iteration = 0;
-
-  clearInterval(interval);
-
-  interval = setInterval(() => {
-    hashingText.innerText = hashingText.innerText
-      .split("")
-      .map((letter, index) => {
-        return letters[Math.floor(Math.random() * letters.length)];
-      })
-      .join("");
-
-    iteration += 1 / 3;
-  }, 100);
-}
-  */
