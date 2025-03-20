ebb08967d538dfa2ad4b6f6969e09bea7293f37c
PhilipHellerupAAU <pjorge24@student.aau.dk>
2025-03-20 11:56:06 +0100

    Changed Port

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.use("/", router);
 
 // Websockets:
 
-const wss = new WebSocketServer({ port: 4310 });
+const wss = new WebSocketServer({ port: 4315 });
 
 
 wss.on("connection", function connection(ws) {
@@ -69,10 +69,10 @@ app.get("/", (req, res) => {
 // Start The Server on the Specified Port (x = 1 (SERVER RAN) or x = 2 (LOCALHOST)):
 let x = 1;
 if (x === 1) {
-  const PORT = 3310;
+  const PORT = 3315;
   server.listen(PORT, "0.0.0.0", () => {
     console.log(
-      "🚀 Server is listening on https://cs-25-sw-2-01.p2datsw.cs.aau.dk/node0/"
+      "🚀 Server is listening on https://cs-25-sw-2-01.p2datsw.cs.aau.dk/node5/"
     );
   });
 } else if (x === 2) {
