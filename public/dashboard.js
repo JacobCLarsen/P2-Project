4e176f938a5d6de2aac8f861d60b508b777fa49e
JacobCLarsen <jcla24@student.aau.dk>
2025-03-20 11:59:25 +0100

    Added a way to track completed tasks since restart

--- public/dashboard.js
+++ public/dashboard.js
@@ -13,14 +13,24 @@ mySocket.addEventListener("open", () => {
 });
 
 // Get elements from the dom
+const onlineWorkersField = document.getElementById("online-workers");
 const activeWorkersField = document.getElementById("active-workers");
+const completedTasksField = document.getElementById("completed-tasks");
 
+// Receive message from the server to update online workes, active workers, and completed tasks in total since server restart
 mySocket.onmessage = (event) => {
   let message = JSON.parse(event.data);
 
   switch (message.action) {
     case "updateOnlineUsers":
-      activeWorkersField.innerText = `Active Workers: ${message.users}`;
+      onlineWorkersField.innerText = `Online workers: ${message.users}`;
+      break;
+
+    case "updateActiveWorkers":
+      break;
+
+    case "updateCompletedTasks":
+      completedTasksField.innerText = `Completed tasks since restart: ${message.count}`;
       break;
 
     default:
--- serverWebsocket.js
+++ serverWebsocket.js
@@ -1,6 +1,7 @@
 // Keep track of online users and client roles
 let workerClientns = [];
 let dashboardClients = [];
+let completedTaskCount = 0;
 
 export function WebsocketListen(ws, wss) {
   ws.onmessage = (event) => {
@@ -9,25 +10,7 @@ export function WebsocketListen(ws, wss) {
     switch (message.action) {
       case "connect":
         console.log(`worker connected with id: ${message.id}`);
-
-        // If the client specifies itself as a dashboard, add it to the dashboardClients list
-        if (message.role === "dashboard") {
-          dashboardClients.push(ws);
-        } else {
-          workerClientns.push(message.id);
-        }
-
-        // Notify only dashboard clients about the updated number of online users
-        dashboardClients.forEach((client) => {
-          if (client.readyState === client.OPEN) {
-            client.send(
-              JSON.stringify({
-                action: "updateOnlineUsers",
-                users: workerClientns.length,
-              })
-            );
-          }
-        });
+        updateOnlineUsers();
         break;
 
       case "request task":
@@ -40,6 +23,9 @@ export function WebsocketListen(ws, wss) {
 
       case "send result":
         console.log(`Result from worker received: ${message.data}`);
+        // TODO: Add a check to see if the task was completed correctly or not
+        completedTaskCount++;
+        updateCompletedTasks();
         break;
 
       case "disconnect":
@@ -69,9 +55,44 @@ export function WebsocketListen(ws, wss) {
 }
 
 // function to create a random task to use in this file
-export function createTask() {
+function createTask() {
   let task = {};
   task.id = Math.floor(Math.random() * 1000);
   task.hash = `0x${Math.random().toString(36).slice(2, 11)}`;
   return task;
+}
+
+// Function to update the number of online users and send it to the dashboard clients
+function updateOnlineUsers() {
+  if (message.role === "dashboard") {
+    dashboardClients.push(ws);
+  } else {
+    workerClientns.push(message.id);
+  }
+
+  // Notify only dashboard clients about the updated number of online users
+  dashboardClients.forEach((client) => {
+    if (client.readyState === client.OPEN) {
+      client.send(
+        JSON.stringify({
+          action: "updateOnlineUsers",
+          users: workerClientns.length,
+        })
+      );
+    }
+  });
+}
+
+// Function to update completed tasks and send it to the dashboard clients
+function updateCompletedTasks() {
+  dashboardClients.forEach((client) => {
+    if (client.readyState === client.OPEN) {
+      client.send(
+        JSON.stringify({
+          action: "updateCompletedTasks",
+          count: completedTaskCount,
+        })
+      );
+    }
+  });
 }
