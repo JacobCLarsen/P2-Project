5640c3eaaed4d5f166b01746f9125c1942555190
Daniel Sloth <131558433+dani156f@users.noreply.github.com>
2025-03-21 10:36:10 +0100

    workerpage ws

--- server.js
+++ server.js
@@ -72,7 +72,7 @@ if (x === 1) {
   const PORT = 3311;
   server.listen(PORT, "0.0.0.0", () => {
     console.log(
-      "🚀 Server is listening on https://cs-25-sw-2-01.p2datsw.cs.aau.dk/node0/"
+      "🚀 Server is listening on https://cs-25-sw-2-01.p2datsw.cs.aau.dk/node1/"
     );
   });
 } else if (x === 2) {
