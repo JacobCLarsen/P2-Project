4f9a651f4f98131f145b21a19e43c2b4b021f2d2
Daniel Sloth <131558433+dani156f@users.noreply.github.com>
2025-03-24 14:01:04 +0100

    dictionary

--- serverWebsocket.js
+++ serverWebsocket.js
@@ -6,6 +6,7 @@ let dashboardClients = [];
 let completedTaskCount = 0;
 let taskQueue = [];
 const dictionaryPath = "./public/dictionary.txt";
+const targetHash = "3c9909afec25354d551dae21590bb26e38d53f2173b8d3dc3eee4c047e7ab1c1eb8b85103e3be7ba613b31bb5c9c36214dc9f14a42fd7a2fdb84856bca5c44c2";
 
 export function WebsocketListen(ws, wss) {
   ws.onmessage = (event) => {
