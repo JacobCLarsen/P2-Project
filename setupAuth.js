e4141f23b19b2ce5679753859ccfa70b4bc6e1a8
ViktorParkhoi <vparkh24@student.aau.dk>
2025-03-25 00:28:14 +0100

    Debug

--- setupAuth.js
+++ setupAuth.js
@@ -50,8 +50,7 @@ export function setupAuth(app) {
             expiresIn: process.env.JWT_EXPIRES_IN,
           }
         );
-        console.log("JWT_SECRET", process.env.JWT_SECRET);
-        console.log("JWT_EXPIRES_IN", process.env.JWT_EXPIRES_IN);
+        console.log("Token:", token);
         res.json({ success: true, message: "Login successful!", token });
       } else {
         res
