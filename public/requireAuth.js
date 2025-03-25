a069d2f978070c8f4b0ed116b4d13750d1f40341
ViktorParkhoi <vparkh24@student.aau.dk>
2025-03-25 18:12:44 +0100

    Update requireAuth.js

--- public/requireAuth.js
+++ public/requireAuth.js
@@ -2,7 +2,10 @@ document.addEventListener("DOMContentLoaded", () => {
   const token = localStorage.getItem("token");
 
   if (!token) {
-    // Redirect to login if no token is found
-    window.location.href = "/html/login";
+    // Extract base path dynamically
+    const basePath = window.location.pathname.split("/").slice(0, 2).join("/");
+
+    // Redirect to login page with dynamic base path
+    window.location.href = `${basePath}/login`;
   }
 });
