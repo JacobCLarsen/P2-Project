1d29d25df74785619c58993abf71af091c941473
ViktorParkhoi <vparkh24@student.aau.dk>
2025-03-25 18:04:25 +0100

    Update requireAuth.js

--- public/requireAuth.js
+++ public/requireAuth.js
@@ -1,8 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
   const token = localStorage.getItem("token");
+  const basePath = path.join(__dirname, "./public/html"); // Going up one level to the root, then to 'public/html'
 
   if (!token) {
     // Redirect to login if no token is found
-    window.location.href = "/login";
+    window.location.href = `${basePath}/login`;
   }
 });
