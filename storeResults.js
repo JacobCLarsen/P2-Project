66f3dfaf28234048c9d69b95c78c93cecbb45189
JacobCLarsen <jcla24@student.aau.dk>
2025-04-03 14:29:42 +0200

    database table and route set up to store weak passwords

--- storeResults.js
+++ storeResults.js
@@ -1,9 +1,42 @@
 import fs from "fs";
 import path from "path";
 
+/* ----- Store_results: (Store weak password in the database) ----- */
+/**
+ * Method: POST
+ * URL: /store_results
+ * Request Body: { weak_passwords: weakPasswords, task_id: taskID }
+ */
+app.post("/store_results", async (req, res) => {
+    const { weakPasswords, taskId } = req.body; // Extract weakPasswords and taskId from the request body
+
+    if (!weakPasswords || !taskId) {
+        return res
+            .status(400) // Respond with "Bad Request" if weakPasswords or taskId is missing
+            .json({ success: false, message: "Missing results" });
+    }
+
+    // Iterate over weakPasswords and insert each hash into the database
+    weakPasswords.forEach((hash) => {
+        const insertQuery =
+            "INSERT INTO weak_passwords (password_hash, taskId) VALUES (?, ?)";
+        DBConnection.query(insertQuery, [hash, taskId], (err, result) => {
+            if (err) {
+                console.error("❌ Error inserting password:", err); // Log error if query fails
+                return res
+                    .status(500)
+                    .json({ success: false, message: "Internal server error" }); // Respond with "Internal Server Error"
+            }
+        });
+    });
+
+    // Respond with success after processing all weakPasswords
+    res.json({ success: true, message: "Passwords logged successfully!" });
+});
+
 // Filepath to text element
 const filepath = path.join(process.cwd(), "./results.txt");
-
+// Function to store the result in database
 function storeResult(task) {
   // Pull the results from the task
   const results = task.results;
