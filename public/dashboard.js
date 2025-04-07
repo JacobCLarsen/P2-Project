23e9c7482870dc086e549ae9b91e5771509274d9
JacobCLarsen <jcla24@student.aau.dk>
2025-04-07 10:12:50 +0200

    changed ports to prepare push to main

--- public/dashboard.js
+++ public/dashboard.js
@@ -1,5 +1,5 @@
 // Create a socket client and connect to the server
-const mySocket = new WebSocket("wss://cs-25-sw-2-01.p2datsw.cs.aau.dk/ws7/");
+const mySocket = new WebSocket("wss://cs-25-sw-2-01.p2datsw.cs.aau.dk/ws1/");
 
 mySocket.addEventListener("open", () => {
   // Send a message to identify this client as a dashboard
@@ -20,7 +20,7 @@ const completedTasksField = document.getElementById("completed-tasks");
 // Receive message from the server to update online workes, active workers, and completed tasks in total since server restart
 mySocket.onmessage = (event) => {
   let message = JSON.parse(event.data);
-console.log("Received message:", message); // Log the entire message for debugging
+  console.log("Received message:", message); // Log the entire message for debugging
 
   switch (message.action) {
     case "updateOnlineUsers":
--- server.js
+++ server.js
@@ -8,7 +8,6 @@ import { fileURLToPath } from "url";
 // Third-party modules
 import express from "express";
 import { createServer } from "http";
-import { Server } from "socket.io";
 
 // Custom modules
 import router from "./router.js";
@@ -42,7 +41,7 @@ app.use(express.json());
 app.use("/", router);
 
 // Websockets:
-const wss = new WebSocketServer({ port: 4317 });
+const wss = new WebSocketServer({ port: 4311 });
 
 wss.on("connection", function connection(ws) {
   console.log("connected");
@@ -69,7 +68,7 @@ app.get("/", (req, res) => {
 // Start The Server on the Specified Port (x = 1 (SERVER RAN) or x = 2 (LOCALHOST)):
 let x = 1;
 if (x === 1) {
-  const PORT = 3317;
+  const PORT = 3311;
   server.listen(PORT, "0.0.0.0", () => {
     console.log(
       "🚀 Server is listening on https://cs-25-sw-2-01.p2datsw.cs.aau.dk/node1/"
