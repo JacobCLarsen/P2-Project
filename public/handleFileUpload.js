92c264df4d918d172dcd0ee22848c9e50d94bea5
JacobCLarsen <jcla24@student.aau.dk>
2025-04-07 12:57:35 +0200

    check hash lenghts

--- public/handleFileUpload.js
+++ public/handleFileUpload.js
@@ -22,9 +22,9 @@ export function uploadFiles(form) {
 export function validateFileUpload(fileList) {
   const allowedTypes = ["text/csv"];
 
+  // Check each file if they are the right filetype
   for (const file of fileList) {
     const { name: fileName } = file;
-
     if (!allowedTypes.includes(file.type)) {
       throw new Error(
         `❌ File "${fileName}" could not be uploaded. Only images with the following types are allowed: .csv`
@@ -33,17 +33,38 @@ export function validateFileUpload(fileList) {
       return true; // Everything is okay
     }
   }
+
+  // Check if the hashes are 512 bits (corresponding to the SHA1-512)
+  chechHashLengths(fileList);
 }
 
 export async function calculateHashCount(fileList) {
-    let totalHashCount = 0;
+  let totalHashCount = 0;
 
-    for (const file of fileList) {
-        const content = await file.text();
-        const lines = content.split("\n");
-        totalHashCount += lines.filter((line) => line.trim() !== "").length;
+  for (const file of fileList) {
+    const content = await file.text();
+    const lines = content.split("\n");
+    totalHashCount += lines.filter((line) => line.trim() !== "").length;
+  }
+
+  console.log(`Number of hashes in file(s): ${totalHashCount}`);
+  return totalHashCount;
+}
+
+// ----------- Helper functions------------
+
+// Chech length of hashes
+async function chechHashLengths(fileList) {
+  for (const file of fileList) {
+    const content = await file.text();
+    const lines = content.split("\n");
+    const hashes = lines.split(",");
+    const validHashes = hashes.filter((hash) => hash.trim().length === 128);
+
+    if (lines.length > validHashes.length) {
+      throw new Error(
+        `Not all lines had a valied 512 bit hash, please check content of file: ${file} lines: ${lines.length}, valied 512 bit hashes: ${validHashes.length}`
+      );
     }
-
-    console.log(`Number of hashes in file(s): ${totalHashCount}`);
-    return totalHashCount;
+  }
 }
