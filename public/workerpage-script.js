5de30b9faaabe64c647af5dc40a78db09412d600
JacobCLarsen <jcla24@student.aau.dk>
2025-04-07 10:37:09 +0200

    css

--- public/workerpage-script.js
+++ public/workerpage-script.js
@@ -7,7 +7,7 @@ const taskQueue = document.getElementById("taskqueue");
 const taskResults = document.getElementById("taskResults");
 const statusMessage = document.getElementById("workstatus");
 const plusTabBtn = document.getElementById("plus-tab-btn");
-const importTaskBtn = document.getElementById("addUsertask");
+const importTaskBtn = document.getElementById("addUsertaskBtn");
 const newTaskBtn = document.getElementById("newTaskBtn");
 const clearQueueBtn = document.getElementById("clearQueueBtn");
 const latestCompletedTask = document.getElementById("latestCompletedTask");
@@ -44,9 +44,7 @@ window.addEventListener("beforeunload", () => {
 });
 
 // Evenetlistene for the "import task butten", which lets a user upload a file with hashed passwords to crack
-importTaskBtn.addEventListener("click", () => [
-  handleFileUpload()
-])
+importTaskBtn.addEventListener("click", () => [handleFileUpload()]);
 
 // When "new task" btn is clicked, a message is sent to the server to create a new task and add it to the taskQueue
 newTaskBtn.addEventListener("click", () => {
