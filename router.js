cbe0a4bca59cf5e724ea83a5672c9b6ba7b98631
Daniel Sloth <131558433+dani156f@users.noreply.github.com>
2025-04-10 12:20:17 +0200

    fix?

--- router.js
+++ router.js
@@ -60,11 +60,6 @@ router.get("/converter", (req, res) => {
   res.sendFile(path.join(basePath, "converter.html"));
 });
 
-// Route for the leaderboard page
-router.get("/leaderboard", (req, res) => {
-  res.sendFile(path.join(basePath, "reward.html"));
-});
-
 // -------------- Post requests --------------
 
 // Handle the post request to upload hashes as a user. Data has been validated on the client side
