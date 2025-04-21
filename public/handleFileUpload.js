3b373ef563f918dd81a21e575caf2310045f9336
JacobCLarsen <jcla24@student.aau.dk>
2025-04-21 11:22:09 +0200

    moves rsaUtils

--- public/handleFileUpload.js
+++ public/handleFileUpload.js
@@ -3,7 +3,7 @@
 // TODO: Encrypt hashes
 // TODO: Be able to drag and drop files into a box to upload
 
-import { rsaUtils } from "./rsaFunction.js";
+import { rsaUtils } from "../rsaFunction.js";
 
 // Function to toggle visibility of a DOM object
 export function toggleVisibility(object, displayStyle) {
