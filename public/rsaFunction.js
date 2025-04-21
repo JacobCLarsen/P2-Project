0625b8f771a0f47c4555ba70c6e05c52ea4e314f
JacobCLarsen <jcla24@student.aau.dk>
2025-04-21 12:43:42 +0200

    test

--- public/rsaFunction.js
+++ public/rsaFunction.js
@@ -2,8 +2,7 @@
 const crypto = window.CryptoJS;
 
 // Test keys for testing
-export const publicKey =
-  "-----BEGIN PUBLIC KEY-----MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA7a9fZV6F5mpzT+jgnStZ6lVjP6Wj8X9c8r16pF+zbgzjmbN5Q/sVXlk5PAvmSTfs2po6Sx7tOBa1LfjyUEUlHUl2b6D6I4lV7M8vvS/fuG1B2SH3fVgZyC5jrTL/sDtrCS4lf64fhKl2zS5Tkc6qHiw+7pYFosqjMY5PS0XsBvqOVqmfec9KHc1h1mIjS5J0F7Vh48Wh3MfJ04lZZvPzIq5r1/NQ5pFOPRmJw6QjMmXghmJMjk7icn0wr5t3Xkn/jFlA+LFh2Rvhk3/FdZxL3TZwSmxZ2JcmGj+VxNlZcmmlNOtr2E5+YZZnC3DkCAaBN7FhR94pNxZP3zVhb4WqfwIDAQAB-----END PUBLIC KEY-----";
+export const publicKey = "1234567890abcdef";
 export const privateKey = "abcd1234";
 
 export const rsaUtils = {
