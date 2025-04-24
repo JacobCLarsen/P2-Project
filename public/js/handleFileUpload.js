8ce982e8c5da9ad6fa1fdd42b2d02addc6725f51
JacobCLarsen <jcla24@student.aau.dk>
2025-04-24 12:08:12 +0200

    Update handleFileUpload.js

--- public/js/handleFileUpload.js
+++ public/js/handleFileUpload.js
@@ -1,8 +1,6 @@
 // TODO: Add a way to track wich user uploaded a task, to store the results in a database with their id. The user can then reference their results at a later time
 // TODO: Be able to drag and drop files into a box to upload
 
-import e from "cors";
-
 // Function to toggle visibility of a DOM object
 export function toggleVisibility(object, displayStyle) {
   if (object.style.display === "none" || object.style.display === "") {
