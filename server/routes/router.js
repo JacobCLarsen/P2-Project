534b385fa004b5531e2c88ff14396f3356447636
JacobCLarsen <jcla24@student.aau.dk>
2025-04-24 12:17:14 +0200

    Update router.js

--- server/routes/router.js
+++ server/routes/router.js
@@ -9,7 +9,7 @@ import { createTask } from "../tasks/createTask.js";
 import { startNewTask } from "../tasks/startNewtask.js";
 
 // Add a socket connection to the router page
-const mySocket = new WebSocket("wss://cs-25-sw-2-01.p2datsw.cs.aau.dk/ws1/");
+const mySocket = new WebSocket("wss://cs-25-sw-2-01.p2datsw.cs.aau.dk/ws2/");
 
 const router = Router();
 
