9c07d8d3bc029c3a387d0022d510fd9c1fbea3ca
JacobCLarsen <jcla24@student.aau.dk>
2025-04-24 11:13:34 +0200

    comments

--- server/tasks/createTask.js
+++ server/tasks/createTask.js
@@ -1,3 +1,6 @@
+// The task object holds all information about a task, and is added to the main queue
+// It holds an array of hashes from the user, the number of hashes, the number of batches to split the task
+// as well as some variables to keep track of the task status and complesion
 class Task {
   constructor(hashes, numberBatches) {
     this.hashes = hashes;
@@ -5,20 +8,20 @@ class Task {
     this.numberBatches = numberBatches;
     this.subTasksCompleted = 0;
     this.completed = 0;
-    this.id = Math.floor(10000000 + Math.random() * 90000000);
+    this.id = Math.floor(10000000 + Math.random() * 90000000); // ID to keep track of task complesion
     this.results = [];
   }
+
+  // Function to mark the task as completed
   complete() {
     this.completed = 1;
   }
 }
 
+// Constructor of the a new task, from a list of hashes
 export function createTask(hashes) {
-  // Divide into chuncks of 100 hashes
-  const numBatches = Math.ceil(hashes.length / 50);
-
-  // Create a new task with the given hashes and number of batches
-  const newTask = new Task(hashes, numBatches);
+  const numBatches = Math.ceil(hashes.length / 50); // Divide into chuncks of a given length
+  const newTask = new Task(hashes, numBatches); // Create a new task with the given hashes and number of batches
 
   return newTask;
 }
