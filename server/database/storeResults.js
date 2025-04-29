27a71b4c64fbfbb8e2e092de6b78fa05fe96c7b8
ViktorParkhoi <vparkh24@student.aau.dk>
2025-04-29 11:29:41 +0200

    Corrected copilot mistake

--- server/database/storeResults.js
+++ server/database/storeResults.js
@@ -1,7 +1,7 @@
 import fs from "fs";
 import path from "path";
 
-import { authenticateJWT } from "../middleware/middleware_jwt";
+import { authenticateJWT } from "../middleware/middleware_jwt.js"; // Corrected path
 
 /* ----- Store_results: (Store weak password in the database) ----- */
 /**
