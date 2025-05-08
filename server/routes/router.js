686597c5f3e47422e3d65fd2de5979fed64b8360
JacobCLarsen <jcla24@student.aau.dk>
2025-05-08 12:57:39 +0200

    Update router.js

--- server/routes/router.js
+++ server/routes/router.js
@@ -10,8 +10,8 @@ import { createTask } from "../tasks/createTask.js";
 import { startNewTask } from "../tasks/startNewtask.js";
 
 //Port Number Import
-import { getPortNumber } from "./port.js"; 
-getPortNumber()
+import { NODENumber } from "../port.js"; 
+
 
 // Add a socket connection to the router page
 const mySocket = new WebSocket(`wss://cs-25-sw-2-01.p2datsw.cs.aau.dk/ws${NODENumber}/`);
