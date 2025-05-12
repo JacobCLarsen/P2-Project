c94745add6531d1134476156f1353b13f415af83
PhilipHellerupAAU <pjorge24@student.aau.dk>
2025-05-12 13:04:05 +0200

    Added comments
    Added comments

--- server/middleware/middleware_jwt.js
+++ server/middleware/middleware_jwt.js
@@ -1,16 +1,23 @@
+// Import the jsonwebtoken library for handling JWT operations
 import jwt from "jsonwebtoken";
 
+// Function to authenticate a JWT token
 export const authenticateJWT = (token) => {
   return new Promise((resolve, reject) => {
+    // Check if the token is provided
     if (!token) {
-      return reject(new Error("No token provided"));
+      return reject(new Error("No token provided")); // Reject if no token is provided
     }
-    console.log(token);
+
+    console.log(token); // Log the token for debugging purposes
+
+    // Verify the token using the secret key
     jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
       if (err) {
-        return reject(new Error("Invalid or expired token."));
+        return reject(new Error("Invalid or expired token.")); // Reject if the token is invalid or expired
       }
-      resolve(decoded); // Successfully authenticated
+
+      resolve(decoded); // Resolve with the decoded token if authentication is successful
     });
   });
 };
