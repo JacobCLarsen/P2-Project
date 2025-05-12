205eed456a23c40f18b31abca4c70092106a2f4c
JacobCLarsen <jcla24@student.aau.dk>
2025-05-12 13:46:56 +0200

    Update workerpage-script.js

--- public/js/workerpage-script.js
+++ public/js/workerpage-script.js
@@ -73,7 +73,7 @@ uploadForm.addEventListener("change", async (e) => {
   // Check if hashes are valid, and return any valid hashes
   await validateFileUpload(fileList)
     .then(({ validHashes, invalidHashes, fileList }) => {
-      uploadMessage.innerText = "Files selected:", fileList;
+      uploadMessage.innerText = `Files selected: ${fileList}`;
       uploadHashCount.innerHTML = `${validHashes.length} valid hashes of ${
         validHashes.length + invalidHashes.length
       }`;
